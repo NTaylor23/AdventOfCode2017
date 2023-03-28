f3c7796a9da94beb8b882d49d822f0cda2d91adf
NT <c0502986@online.camosun.ca>
2023-03-27 20:33:24 -0700

    day15a fixed

--- day15a.py
+++ day15a.py
@@ -1,4 +1,4 @@
 from cython_files import day15a_cy
 
 result = day15a_cy.calculate(289, 629, 16807, 48271)
-print(result)
+print(f'Day 14 Part 1: After 40 million pairs, the judge\'s final count is {result}.\n')
