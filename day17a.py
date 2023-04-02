d9c75a6d54ef04fdf088c55e4a547972aadc6394
NT <c0502986@online.camosun.ca>
2023-04-01 19:54:41 -0700

    day 17 p1

--- day17a.py
+++ day17a.py
@@ -3,14 +3,18 @@ class Node:
         self.val = val
         self.next = None
 
-class SpinLock:
-    # Make this a CIRCULAR LINKED LIST...
-    def __init__(self) -> None:
-        self.head = None
-        self.count = 0
-        self.directory = {}
 
-    def insert(self, val: int, after_target: int):
+class SpinLock:
+    
+    def __init__(self) -> None:
+        self.head = Node(0)
+        self.head.next = self.head
+        self.count = 1
+        self.step_size = 366
+        self.directory = { 0: self.head }
+
+    def insert(self, val: int, after_target: int) -> Node:
+        self.count += 1
         new_node = Node(val)
 
         prev_node = self.get_node(after_target)
@@ -18,28 +22,22 @@ class SpinLock:
         prev_node.next = new_node
 
         self.directory[val] = new_node
+        return new_node
 
-    def get_node(self, val):
-        if not self.directory[val]:
+    def get_node(self, val) -> Node:
+        if val not in self.directory:
             raise KeyError("?????")
         return self.directory[val]
 
-    def append(self, val):
-        new_node = Node(val)
-        self.directory[val] = new_node
+    def iterate(self, current_position, next_val) -> Node:
+        pos = self.get_node(current_position)
+        for _ in range(self.step_size):
+            pos = pos.next
+        return self.insert(next_val, pos.val)
 
-        if not self.head:
-            self.head = new_node
-            return
-        
-        last_node = self.head
+spinlock = SpinLock()
+for i in range(1, 2018):
+    spinlock.iterate(i - 1, i)
 
-        while last_node.next:
-            last_node = last_node.next
-
-        last_node.next = new_node
-        self.count += 1
-
-ll = SpinLock()
-ll.append(0) # head
-ll.insert(5, 0)
+result: Node = spinlock.get_node(2017).next.val
+print(f'Day 17 Part 1: The value in my completed cirular buffer after 2017 is {result}.\n')
